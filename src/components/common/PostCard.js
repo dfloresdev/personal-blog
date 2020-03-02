37bd5ff7f55fceed11ddbd0be556ca11f4d69989
dfloresdev <hello@dflores.dev>
2020-03-01 20:10:19 -0600

    [CHANGE] postcard new

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -13,8 +13,9 @@ const PostCard = ({ post }) => {
             <header className="post-card-header">
                 {post.feature_image &&
                     <div className="post-card-image" style={{
-                        backgroundImage: `url(${post.feature_image})` ,
+                        backgroundImage: `url(${post.feature_image})`,
                     }}></div>}
+
                 {post.tags && <div className="post-card-tags"> <Tags post={post} visibility="public" autolink={false} /></div>}
                 {post.featured && <span>Featured</span>}
                 <h2 className="post-card-title">{post.title}</h2>
@@ -24,11 +25,11 @@ const PostCard = ({ post }) => {
                 <div className="post-card-footer-left">
                     <div className="post-card-avatar">
                         {post.primary_author.profile_image ?
-                            <img className="author-profile-image" src={post.primary_author.profile_image} alt={post.primary_author.name}/> :
-                            <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name}/>
+                            <img className="author-profile-image" src={post.primary_author.profile_image} alt={post.primary_author.name} /> :
+                            <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name} />
                         }
                     </div>
-                    <span>{ post.primary_author.name }</span>
+                    <span>{post.primary_author.name}</span>
                 </div>
                 <div className="post-card-footer-right">
                     <div>{readingTime}</div>
